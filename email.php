8c4654a71e2ef3083d8d46fb0b83aee7bd47d46c
Rosa Maria BJ <62024358+Rosaenergyflow@users.noreply.github.com>
2022-09-08 18:46:07 +0200

    Update email.php

--- email.php
+++ email.php
@@ -30,14 +30,14 @@ try {
     $mail->isSMTP();                                            
     $mail->Host       = 'smtp.gmail.com';                    
     $mail->SMTPAuth   = true;                                   
-    $mail->Username   = 'youremail@gmail.com';                     
-    $mail->Password   = 'yourpassword';                               
+    $mail->Username   = 'rosamariabj.dev@gmail.com';                     
+    $mail->Password   = 'Grow2022.';                               
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
     $mail->Port       = 587;                                   
     
     //Recipients
-    $mail->setFrom('youremail@gmail.com', 'title');
-    $mail->addAddress('youremail@gmail.com', 'title');   
+    $mail->setFrom('rosamariabj.dev@gmail.com', 'title');
+    $mail->addAddress('rosamariabj.dev@gmail.com', 'title');   
     
     
     // $mail->addAddress('anyemail@gmail.com@gmail.com');               
